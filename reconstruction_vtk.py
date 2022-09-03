d21f4ab74264826a6318f221f27556ff80518d60
GabrielBueno200 <gabrielbueno200@gmail.com>
2022-09-03 16:39:58 -0300

    refactored reconstruction script

--- reconstruction_vtk.py
+++ reconstruction_vtk.py
@@ -38,6 +38,8 @@ from vtk.vtkRenderingCore import (
 )
 from vtkmodules.vtkRenderingCore import vtkProp3D
 
+colors = vtkNamedColors()
+
 def get_landmarks_actors():
     colors = vtkNamedColors()
 
@@ -59,46 +61,19 @@ def get_landmarks_actors():
 
     return actor
 
-def main(path, iso):
-    # vtkFlyingEdges3D was introduced in VTK >= 8.2
+def get_skull_actor(iso_value, dicom_dir):
     use_flying_edges = vtk_version_ok(8, 2, 0)
 
-    colors = vtkNamedColors()
-
-    dicom_dir = path
-    iso_value = iso
     if iso_value is None and dicom_dir is not None:
         print('An ISO value is needed.')
         return ()
 
     volume = vtkImageData()
-    if dicom_dir is None:
-        # draw a circle
-        sphere_source = vtkSphereSource()
-        sphere_source.SetPhiResolution(20)
-        sphere_source.SetThetaResolution(20)
-        sphere_source.Update()
 
-        bounds = list(sphere_source.GetOutput().GetBounds())
-        for i in range(0, 6, 2):
-            dist = bounds[i + 1] - bounds[i]
-            bounds[i] = bounds[i] - 0.1 * dist
-            bounds[i + 1] = bounds[i + 1] + 0.1 * dist
-        voxel_modeller = vtkVoxelModeller()
-        voxel_modeller.SetSampleDimensions(50, 50, 50)
-        voxel_modeller.SetModelBounds(bounds)
-        voxel_modeller.SetScalarTypeToFloat()
-        voxel_modeller.SetMaximumDistance(0.1)
-
-        voxel_modeller.SetInputConnection(sphere_source.GetOutputPort())
-        voxel_modeller.Update()
-        iso_value = 0.5
-        volume.DeepCopy(voxel_modeller.GetOutput())
-    else:
-        reader = vtkDICOMImageReader()
-        reader.SetDirectoryName(dicom_dir)
-        reader.Update()
-        volume.DeepCopy(reader.GetOutput())
+    reader = vtkDICOMImageReader()
+    reader.SetDirectoryName(dicom_dir)
+    reader.Update()
+    volume.DeepCopy(reader.GetOutput())
 
     if use_flying_edges:
         try:
@@ -108,45 +83,38 @@ def main(path, iso):
     else:
         surface = vtkMarchingCubes()
 
-    landmarksActor = get_landmarks_actors()
-
     surface.SetInputData(volume)
     surface.ComputeNormalsOn()
     surface.SetValue(0, iso_value)
-
-    renderer = vtkRenderer()
-    renderer.SetBackground(colors.GetColor3d('Black'))
-
-    render_window = vtkRenderWindow()
-    render_window.AddRenderer(renderer)
-    render_window.SetWindowName('Cranial Reconstruction: ' + path)
-    render_window.SetSize(1500, 800)
-
-    interactor = vtkRenderWindowInteractor()
-    interactor.SetRenderWindow(render_window)
 
     mapper = vtkPolyDataMapper()
     mapper.SetInputConnection(surface.GetOutputPort())
     mapper.ScalarVisibilityOff()
 
-    skullActor = vtkActor()
-    skullActor.SetMapper(mapper)
-    skullActor.GetProperty().SetColor(colors.GetColor3d('Green'))
+    actor = vtkActor()
+    actor.SetMapper(mapper)
+    actor.GetProperty().SetColor(colors.GetColor3d('Green'))
 
-    # pointPicker = vtk.vtkPointPicker()
-    # pointPicker.PickFromListOn()
-    # try:
-    #     pointPicker.SetUseCells(True)
-    # except:
-    #     print("Warning, vtkPointPicker patch not installed, picking will not work properly.")
+    return actor
 
-    renderer.AddActor(skullActor)
-    renderer.AddActor(landmarksActor)
+
+def main(dicom_dir, iso_value):
+    renderer = vtkRenderer()
+    renderer.SetBackground(colors.GetColor3d('Black'))
+
+    render_window = vtkRenderWindow()
+    render_window.AddRenderer(renderer)
+    render_window.SetWindowName('Cranial Reconstruction: ' + dicom_dir)
+    render_window.SetSize(1500, 800)
+
+    interactor = vtkRenderWindowInteractor()
+    interactor.SetRenderWindow(render_window)
+
+    renderer.AddActor(get_skull_actor(iso_value, dicom_dir))
+    renderer.AddActor(get_landmarks_actors())
 
     renderer.ResetCamera()
 
-#    pointPicker.InitializePickList()
-#    interactor.SetPicker(pointPicker)
     render_window.Render()
     interactor.Start()
 
@@ -164,11 +132,5 @@ def vtk_version_ok(major, minor, build):
 
     vtk_version_number = 10000000000 * vtk.VTK_MAJOR_VERSION + 100000000 * vtk.VTK_MINOR_VERSION \
                          + vtk.VTK_BUILD_VERSION
-    if vtk_version_number >= needed_version:
-        return True
-    else:
-        return False
 
-
-if __name__ == '__main__':
-    main()
+    return vtk_version_number >= needed_version
